49a2cb78146a4ed7a884816de5243fe05a697604
jaeyeonling <jaeyeonling@gmail.com>
2020-09-20 00:37:04 +0900

    Add git open

--- yarn/install.sh
+++ yarn/install.sh
@@ -5,6 +5,7 @@ yarn global add create-html5-boilerplate
 yarn global add fkill-cli
 yarn global add gatsby-cli
 yarn global add gh-pages
+yarn global add git-open
 yarn global add pm2
 yarn global add serverless
 yarn global add speed-test
